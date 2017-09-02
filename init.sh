2eb97d6f58c03a83fa55b7f413a09ea72b87dd02
ne_Sachirou <utakata.c4se@gmail.com>
2017-09-03 03:41:37 +0900

    init.sh for Darwin (MacOS) & Linux (Ubuntu)

--- init.sh
+++ init.sh
@@ -1,9 +1,35 @@
 #!/bin/bash -eux
 
-if ! which brew &> /dev/null ; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-brew update
-brew install ansible
+# Shell & Utilities: Table of Contents http://pubs.opengroup.org/onlinepubs/009696699/utilities/contents.html
+
+function install_ansible_by_apt() {
+  $(with_sudo apt) update
+  $(with_sudo apt) install ansible
+}
+
+function install_ansible_by_brew() {
+  brew update
+  brew install ansible
+}
+
+function with_sudo() {
+  if test "" = "$(find "$(which "$1")" -user root)" ; then
+    echo "$1"
+  else
+    echo "sudo $1"
+  fi
+}
+
+case $(uname -s) in
+  Darwin*)
+    if ! which brew &> /dev/null ; then
+      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+    install_ansible_by_brew
+    ;;
+  Linux*)
+    install_ansible_by_apt
+    ;;
+esac
 mkdir -p ~/dotfiles
 git clone --depth=1 git@github.com:ne-sachirou/dotfiles.git ~/dotfiles
