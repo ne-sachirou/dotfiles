967b102ea65c8271e922b136eb0d7a4c0e3e86d5
ne-sachirou <ne-sachirou@hatena.ne.jp>
2020-01-07 22:39:06 +0900

    Shell scriptは成るべくPOSIX互換に書く

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
-# Single UNIX Specification (SUSv3) Shell and Utilities (XCU) http://pubs.opengroup.org/onlinepubs/009696699/utilities/contents.html
+# Shell scriptは成るべくPOSIX互換に書く - .｡oO(さっちゃんですよヾ(〃l _ l)ﾉﾞ☆) https://scrapbox.io/ne-sachirou/Shell_script%E3%81%AF%E6%88%90%E3%82%8B%E3%81%B9%E3%81%8FPOSIX%E4%BA%92%E6%8F%9B%E3%81%AB%E6%9B%B8%E3%81%8F
 
 function install_ansible_by_apt() {
   $(with_sudo apt) update
