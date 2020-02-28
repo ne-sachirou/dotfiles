f95e5c37aaa1bdabc1c5e33e8bc4702fe6fdb9a4
ne-sachirou <ne-sachirou@hatena.ne.jp>
2020-02-28 15:52:52 +0900

    `shellcheck $0` on running shell script

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 
 # Shell scriptは成るべくPOSIX互換に書く - .｡oO(さっちゃんですよヾ(〃l _ l)ﾉﾞ☆) https://scrapbox.io/ne-sachirou/Shell_script%E3%81%AF%E6%88%90%E3%82%8B%E3%81%B9%E3%81%8FPOSIX%E4%BA%92%E6%8F%9B%E3%81%AB%E6%9B%B8%E3%81%8F
 
+shellcheck "$0"
+
 function install_ansible_by_apt() {
   $(with_sudo apt) update
   $(with_sudo apt) install ansible
