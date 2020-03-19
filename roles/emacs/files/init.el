b076040b6625d71f95435f3084cace261afc25e5
ne-sachirou <ne-sachirou@hatena.ne.jp>
2020-03-19 17:58:55 +0900

    s/tide/typescript-language-server/

--- roles/emacs/files/init.el
+++ roles/emacs/files/init.el
@@ -60,7 +60,6 @@
     slim-mode
     solidity-mode
     terraform-mode
-    tide
     typescript-mode
     vimrc-mode
     yaml-mode
@@ -186,27 +185,10 @@
 ;; company-terraform
 (company-terraform-init)
 
-;; tide
-(defun setup-tide-mode ()
-  (interactive)
-  (tide-setup)
-  (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (eldoc-mode +1)
-  (tide-hl-identifier-mode +1)
-  (company-mode +1))
-(setq company-tooltip-align-annotations t)
-(add-hook 'before-save-hook 'tide-format-before-save)
-(add-hook 'typescript-mode-hook #'setup-tide-mode)
-(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
-(add-hook 'web-mode-hook
-  (lambda ()
-    (when (string-equal "tsx" (file-name-extension buffer-file-name))
-      (setup-tide-mode))))
-;(flycheck-add-mode 'typescript-tslint 'web-mode)
-
 ;; typescript-mode
-(add-to-list 'auto-mode-alist '("\.tsx\'" . typescript-mode))
+;(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode))
+(add-hook 'typescript-mode-hook #'lsp)
 
 ;; company-web
 (require 'company-web-html)
