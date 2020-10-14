93ed7d22c7aef2d85a9b807aeeb23f4cdd3c1574
ne-sachirou <ne-sachirou@hatena.ne.jp>
2020-10-14 10:50:12 +0900

    s/helm/ivy/

--- roles/emacs/files/init.el
+++ roles/emacs/files/init.el
@@ -14,6 +14,7 @@
     company
     company-terraform
     company-web
+    counsel
     csharp-mode
     dap-mode
     docker-tramp
@@ -34,9 +35,7 @@
     go-mode
     groovy-mode
     haskell-mode
-    helm
-    helm-ag
-    helm-mt
+    ivy
     j-mode
     jinja2-mode
     lsp-metals
@@ -58,6 +57,7 @@
     sbt-mode
     scala-mode
     slim-mode
+    swiper
     terraform-mode
     typescript-mode
     vimrc-mode
@@ -189,6 +189,22 @@
 ;; company-web
 (require 'company-web-html)
 
+;; counsel
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> o") 'counsel-describe-symbol)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+
 ;; csharp-mode
 
 ;; dap-mode
@@ -260,14 +276,12 @@
 
 ;; haskell-mode
 
-;; helm
-(require 'helm-config)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(helm-mode 1)
-
-;; helm-ag
-
-;; helm-mt
+;; ivy
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
 
 ;; j-mode
 
@@ -361,6 +375,8 @@
 
 ;; slim-mode
 
+;; swiper
+(global-set-key "\C-s" 'swiper)
 
 ;; terraform-mode
 
