cd057afa7aaa0873c5340577931d1a08029e3e1b
ne-sachirou <ne-sachirou@hatena.ne.jp>
2021-03-29 11:16:39 +0900

    `xcode-select --install` on init

--- init/macos.sh
+++ init/macos.sh
@@ -1,7 +1,10 @@
-#!/bin/bash -eux
+#!/bin/bash
 
 # Shell scriptは成るべくPOSIX互換に書く - .｡oO(さっちゃんですよヾ(〃l _ l)ﾉﾞ☆) https://scrapbox.io/ne-sachirou/Shell_script%E3%81%AF%E6%88%90%E3%82%8B%E3%81%B9%E3%81%8FPOSIX%E4%BA%92%E6%8F%9B%E3%81%AB%E6%9B%B8%E3%81%8F
 
+set -eux
+
+xcode-select --install
 if ! command -v brew &> /dev/null ; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
